9ca11ddef50b46f511e778d2c2a459d733dc89af
RavalAnkesh <151333058+RavalAnkesh@users.noreply.github.com>
2026-03-31 08:16:34 +0530

    Update thread.java

--- thread.java
+++ thread.java
@@ -1,10 +1,6 @@
 public class thread {
 
-    public static void main(String[] args) {
-
-        // ==============================
-        // 🔹 SINGLE THREAD (only main)
-        // ==============================
+    public static void main
         System.out.println("---- Single Thread ----");
 
         for (int i = 1; i <= 5; i++) {
